6ccfe60430d141171d0b536f94139fd40d3cf142
Yhunghabey <yhung.habey1994@gmail.com>
2022-04-16 22:24:24 +0100

    error fixed

--- app/module/user/model.js
+++ app/module/user/model.js
@@ -36,7 +36,7 @@ const UserSchema = mongoose.Schema({
   },
   phoneNumber: {
     type: String,
-    unique: [true, 'User with phone number already exists'],
+    //unique: [true, 'User with phone number already exists'],
   },
   password: {
     type: String,
